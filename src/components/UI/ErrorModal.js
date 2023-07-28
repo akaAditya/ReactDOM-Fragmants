89b096aab63ed898525e4d8f839f5a3c47d20cb5
akaAditya <mymail.adityapatel@gmail.com>
2023-07-28 20:09:08 +0530

    first commit reactDOM fragmants

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+
+  const BackDrop = (props) =>{
+    return <div className={classes.backdrop} onClick={props.onConfirm} />
+  }
+
+  const ModalOverlay = (props)=>{
+    return (
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    )
+  }
+  const ErrorModal = (props) => {
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<BackDrop onConfirm={props.onCofirm}/>, document.getElementById('backdrop-root'))}
+    </React.Fragment>
+  );
+};
+
+export default ErrorModal;
